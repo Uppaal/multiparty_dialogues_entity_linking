466bd0ca5691ce32adee3c26ff494111be092262
Mili Shah <13bce105@nirmauni.ac.in>
2017-12-21 22:45:45 -0500

    Add dropout, clean entity linking

--- code/entity_linking.py
+++ code/entity_linking.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+
 class EntityLinking(object):
 	def __init__(self, embedding_size, n_classes, mode):
 		if mode=='train':
@@ -19,9 +20,8 @@ class EntityLinking(object):
 		self.sess = tf.Session()
 		if self.train_mode:
 			self.sess.run(self.init_op)
+		# self.variables = tf.global_variables()
 
-		self.variables = tf.all_variables()
-	
 
 	def create_placeholders(self):
 		self.mention = tf.placeholder(tf.float32, [None,self.embedding_size], name='mention') 
@@ -33,8 +33,12 @@ class EntityLinking(object):
 		self.y = tf.placeholder(tf.float32, [None,self.n_classes], name='y')
 	
 
-	def get_cnn_layer(self, input_conv, n_filters, filter_size, conv_stride = [1,1]):
-		conv = tf.layers.conv2d(inputs = input_conv,
+	def get_rep_embeddings(self,list_of_embeddings, n_filters, filter_size, conv_stride = [1,1], keep_prob = 0.8):
+		avg_pool = tf.reduce_mean(list_of_embeddings, axis=1, keep_dims=True)
+		max_pool = tf.reduce_max(list_of_embeddings, axis=1, keep_dims=True)
+		pooled = tf.concat([avg_pool, max_pool], axis=1, name='pooled')
+			# batch_size x 2 x embeddings_size x 1
+		conv = tf.layers.conv2d(inputs = pooled,
 								filters = n_filters,
 								kernel_size = filter_size,
 								strides = conv_stride,
@@ -42,34 +46,25 @@ class EntityLinking(object):
 								activation = tf.nn.tanh,
 								name='conv'
 								)
-		return conv
+			# batch_size x 1 x 1 x embeddings_size
+		if self.train_mode:
+			conv = tf.nn.dropout(conv, keep_prob = keep_prob)
+		cluster_emb = tf.reshape(conv, [-1, self.embedding_size], name='cluster_emb')
+			# batch_size x embeddings_size
+		return cluster_emb
 
 	def create_make_embeddings_layer(self):
 		with tf.name_scope('mention_cluster'):
 			with tf.variable_scope('mention_cluster'):
-				avg_pool = tf.reduce_mean(self.mentions_in_cluster, axis=1, keep_dims=True)
-				max_pool = tf.reduce_max(self.mentions_in_cluster, axis=1, keep_dims=True)
-				self.pooled_m = tf.concat([avg_pool, max_pool], axis=1, name='pooled_m')
-					# batch_size x 2 x embeddings_size x 1
-				self.cnn_outs_m = self.get_cnn_layer(input_conv = self.pooled_m,
-													n_filters = self.embedding_size,
-													filter_size = [2,self.embedding_size])
-					# batch_size x 1 x 1 x embeddings_size
-				self.cluster_emb_m = tf.reshape(self.cnn_outs_m, [-1, self.embedding_size], name='cluster_emb_m')
-					# batch_size x embeddings_size
+				self.cluster_emb_m = self.get_rep_embeddings(list_of_embeddings = self.mentions_in_cluster, 
+														n_filters = self.embedding_size,
+														filter_size = [2,self.embedding_size])
 		
 		with tf.name_scope('mention_pairs_cluster'):
 			with tf.variable_scope('mention_pairs_cluster'):
-				avg_pool = tf.reduce_mean(self.mention_pairs_in_cluster, axis=1, keep_dims=True)
-				max_pool = tf.reduce_max(self.mention_pairs_in_cluster, axis=1, keep_dims=True)
-				self.pooled_p = tf.concat([avg_pool, max_pool], axis=1, name='pooled_p')
-					# batch_size x 2 x embeddings_size x 1
-				self.cnn_outs_p = self.get_cnn_layer(input_conv = self.pooled_p,
-													n_filters = self.embedding_size,
-													filter_size = [2,self.embedding_size])
-					# batch_size x embeddings_size x 1 x 1 x 1
-				self.cluster_emb_p = tf.reshape(self.cnn_outs_p, shape = [-1, self.embedding_size], name='cluster_emb_p') 
-					# batch_size x embeddings_size
+				self.cluster_emb_p = self.get_rep_embeddings(list_of_embeddings = self.mention_pairs_in_cluster, 
+														n_filters = self.embedding_size,
+														filter_size = [2,self.embedding_size])
 		
 		self.emb_features = tf.concat([self.mention,self.cluster_emb_m,self.cluster_emb_p], axis=1, name='emb_features') 
 			# batch_size x (3 * embeddings_size)
